1d801db7eea1e008bc02cb659f2ebec3d81a8fb9
Naoldaba <115622877+Naoldaba@users.noreply.github.com>
2023-10-07 18:02:18 +0300

    Update selection_sort.js

--- selection_sort.js
+++ selection_sort.js
@@ -1,20 +1,3 @@
-select(arr,i){
-     // code here such that selectionSort() sorts arr[]
-     for(let m=0;m<arr.length;i++){
-        for(let j=0;j<arr.length;j++){
-            if(arr[m]<arr[j]){
-                let temp=arr[m]
-                arr[m]=arr[j]
-                arr[j]=temp
-            }
-            if(arr[m]==i){
-                break
-            }
-        }
-    }
-    return arr
-  }
-
   //Function to sort the array using selection sort algorithm.
   selectionSort(arr,n){
    //code here
